f973e3de902ac35b69a7810219c19f6551e64033
hfeisal <halimafeisal@gmail.com>
2022-03-17 14:34:12 -0700

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main - prints _putchar, followed by new line
+ *Return: Always 0(Success)
+ */
+int main(void)
+{
+_putchar('_');
+putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+my readme file a function that checks for uppercase character
