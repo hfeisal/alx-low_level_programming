4508e4e4ccd8fbdf63f08f82549c9571e07f5b94
hfeisal <halimafeisal@gmail.com>
2022-03-17 14:58:59 -0700

    checks for case character

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,4 @@
 #ifndef _main_h_
-
 #define _main_h_
 int _putchar(char c);
 int _isupper(int c);
@@ -13,5 +12,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
 #endif
