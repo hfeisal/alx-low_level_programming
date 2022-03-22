12c2a12c814ab4a4e0c2c120cb0f9b7700f34610
hfeisal <halimafeisal@gmail.com>
2022-03-22 14:11:51 -0700

    swap horses

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * swap_int - swaps the values of two integers
  * @a: Integer to be swapped
@@ -7,6 +8,7 @@
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c= *a;
 	*a = *b;
 	*b = c;
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,2 @@
 a function that takes a pointer to an int as parameter and updates the value it points
-Don't swap horses in crossing a stream 
+dont swap horses in crossing a stream 
