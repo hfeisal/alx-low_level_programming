c754e799cd96ee2cd94208e1f378a664b0820b7c
hfeisal <halimafeisal@gmail.com>
2022-03-24 22:45:12 -0700

    two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,14 +16,15 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[i] != '\0')
 		i++;
-
 	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
+
 		i++;
 		j++;
 	}
 
 	dest[i] = '\0';
+
 	return (dest);
-}	
+}
